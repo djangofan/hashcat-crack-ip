96a474587cf4953853f3024a088d3aee27f8a3de
Jon Austen <djangofan@gmail.com>
2018-02-25 19:04:43 -0800

    Minor edit.

--- crackItBruteForce.sh
+++ crackItBruteForce.sh
@@ -5,9 +5,8 @@
 # hash: 3835E0DDAE5C86060874A43A035D3EAA86E976D89B664194F71A8362D243E935
 
 while read pattern; do
-  echo "Trying pattern " + $pattern
+  printf "\nTrying pattern ${pattern}"
   hashcat ./hashedIPs.txt --outfile-format=2 -o outfile.txt -m 1400 -a 3 $pattern
-
 done <brute-force-patterns.txt
 
 echo "Result:"
